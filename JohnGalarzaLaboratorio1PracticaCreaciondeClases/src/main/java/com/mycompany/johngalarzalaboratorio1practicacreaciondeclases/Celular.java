acaf5e7475246cd7d8606db7bcf5fcab296a8730
jfgalarza5 <jfgalarza5@espe.edu.ec>
2023-05-12 21:41:11 -0500

    Laboratorio 1 Practica Creacion de Clases

---
+++ JohnGalarzaLaboratorio1PracticaCreaciondeClases/src/main/java/com/mycompany/johngalarzalaboratorio1practicacreaciondeclases/Celular.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.johngalarzalaboratorio1practicacreaciondeclases;
+
+/**
+ *
+ * CREAR CLASE CELULAR
+ */
+public class Celular {
+    String Marca;
+    String Color;
+    String Modelo;
+    String Memoria;
+    int Precio; 
+}
---
+++ JohnGalarzaLaboratorio1PracticaCreaciondeClases/src/main/java/com/mycompany/johngalarzalaboratorio1practicacreaciondeclases/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.johngalarzalaboratorio1practicacreaciondeclases;
+
+/**
+ *
+ * CREAR CLASE PERSONA
+ * ATRIBUTOS
+ */
+public class Persona {
+    String Etnia;
+    int Edad;
+    String Nacionalidad;
+    String Genero;
+    String Nombre;
+}
